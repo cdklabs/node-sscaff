b2eefe83c19e22991c5c169661839ce22eb2f66a
Elad Ben-Israel <benisrae@amazon.com>
2021-12-05 15:39:50 +0200

    chore: upgrade deps and change default branch to main
    Signed-off-by: Elad Ben-Israel <benisrae@amazon.com>

--- .projenrc.js
+++ .projenrc.js
@@ -1,13 +1,13 @@
-const { TypeScriptProject } = require('projen');
+const { typescript } = require('projen');
 
-const project = new TypeScriptProject({
+const project = new typescript.TypeScriptProject({
   name: 'sscaff',
   description: 'Stupid scaffoling: create a copy of a directory with variable substitution',
   authorName: 'Elad Ben-Israel',
   authorEmail: 'elad.benisrael@gmail.com',
   repository: 'https://github.com/cdklabs/node-sscaff',
   releaseToNpm: true,
-  defaultReleaseBranch: 'master',
+  defaultReleaseBranch: 'main',
   srcdir: 'lib',
   testdir: 'lib/__tests__',
   minNodeVersion: '12.13.0',
